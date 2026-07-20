8a5aee9dfcd64011ab17b0ce418e862b6620d753
YasineDeghaies <yasine.deghaies@gmail.com>
2026-07-20 05:01:46 +0200

    implemented semantic search with cosine distance

--- app.py
+++ app.py
@@ -2,16 +2,39 @@ from sentence_transformers import SentenceTransformer
 import scipy.spatial
 import pandas as pd
 
-examples = [ #A
-"The cat is playing in the garden",
-"A dog and a cat are good pets",
-"Cats love to chase mice",
-"Machine learning is based on algorithms",
-"Deep learning uses neural networks",
-"Recurrent networks have connections"]
+def semantic_search(query_embedding, embeddings, examples):
+    
+    #scores computes the cosine distance, which is different than the cosine similiarty
+        # cosine similiarty = cos(θ) 
+        # cosine distance =  1 - cos(θ)
+    scores = [scipy.spatial.distance.cosine(query_embedding, doc) for doc in embeddings]
 
-#Load the model
-model = SentenceTransformer('all-MiniLM-L6-v2')
-print(model)
+    for i, doc in enumerate(examples):
+        print(f"Example {i}: {examples[i]}")
+        print(f"Score: {1 - scores[i]:.4f}")
 
+def main():
+    examples = [ #A
+    "The cat is playing in the garden",
+    "A dog and a cat are good pets",
+    "Cats love to chase mice",
+    "Machine learning is based on algorithms",
+    "Deep learning uses neural networks",
+    "Recurrent networks have connections"
+    ]
 
+    #load encoder 
+        #is 'all-MiniLM-L6-v2' the encoder's name by default ?  No, we need to specify which encoder to use everytime
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    #create embeddings for the examples
+    embeddings = model.encode(examples)
+
+    #embed user query
+    query = "Machine Learning"
+    query_embedding = model.encode(query) #shape: (384,)
+    
+    semantic_search(query_embedding, embeddings, examples)
+
+if __name__ == "__main__":
+    main()
